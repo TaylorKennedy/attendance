2e03e4cf01db795bbf1150ab0b3093e57eda4afb
Taylor Kennedy <taylor.kennedy1@gmail.com>
2016-08-11 23:35:18 -0500

    attendance can be taken

--- routes/names.js
+++ routes/names.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const names = ['Rick', 'Morty', 'Summer', 'Beth', 'Jerry'];
+const names = {};
 
 router.get('/', (req, res, next) => {
     res.render('names.ejs', {names: names});
 });
 
 router.post('/', (req, res, next) => {
-  const name = req.body.name;
-  names.push(name);
+  const name = req.body.name.trim();
+  if (names[name]){
+    names[name] += 1;
+  }
+  else{
+    names[name] = 1;
+  };
+  //names.push(name);
   res.redirect('/names');
 });
 
